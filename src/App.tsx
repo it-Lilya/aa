ef913e7f07a5e9412e7fb43575e21dc1502b3562
it-Lilya <it_lilya@bk.ru>
2024-07-08 21:49:05 +0300

    sec

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,21 @@
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
-import { store, IncrementAction } from './store';
+import { store, IncrementAction, DecrementAction, CounterId, AppState, useAppSelector, selectCounter } from './store';
+import { useEffect, useReducer, useRef } from 'react';
+import { useDispatch, useSelector, useStore } from 'react-redux';
 
 function App() {
+  // const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  // useEffect(() => {
+  //   // передаем ф которая будет вызываться каждый раз когда летит action store
+  //   const unsubscribe = store.subscribe(() => {
+  //     forceUpdate();
+  //   });
+
+  //   // отписываемся при размонтировании
+  //   return unsubscribe;
+  // })
   return (
     <>
       <div>
@@ -15,23 +27,69 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <div className="card">
-      counter {store.getState().counter}
-        <button onClick={() => store.dispatch({type: 'increment'} satisfies IncrementAction)}>
-          increment
-        </button>
-        <button onClick={() => setCount((count) => count + 1)}>
-          decrement
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
+      <Counter counterId='first'/>
+      <Counter counterId='second' />
       <p className="read-the-docs">
         Click on the Vite and React logos to learn more
       </p>
     </>
   )
 }
+// селектор - чистая ф котор принимает в себя state 
+// и возвращает его какой-то кусочек (не должна создавать 
+// новых объектов, массивов)
 
+// возвраает реф с состоянием или undefined
+// const selectCounter = (state: AppState, counterId: CounterId) => state.counters[counterId];
+
+export function Counter({counterId}: {counterId: CounterId}) {
+  // обращаются к store из контекста
+  // useDispatch();
+  // useStore();
+  // useSelector(); // получ данные из стора, внутри себя реализует логику
+  // проверяет изменились ли селекторы в store, и перерисовывает
+  const dispatch = useDispatch();
+  const counterState = useAppSelector((state) => selectCounter(state, counterId));
+  console.log('render', counterId);
+  /*
+  const [, forceUpdate] = useReducer((x) => x + 1, 0);
+  console.log('render', counterId);
+  const lastStateRef = useRef<ReturnType<typeof selectCounter>>();
+  useEffect(() => {
+    // передаем ф которая будет вызываться каждый раз когда летит action store
+    const unsubscribe = store.subscribe(() => {
+      const currentState = selectCounter(store.getState(), counterId);
+      const lastState = lastStateRef.current;
+
+      // сравнение селектора
+      if (currentState !== lastState) {
+        forceUpdate();
+      }
+      lastStateRef.current = currentState;
+    });
+    // отписываемся при размонтировании
+    return unsubscribe;
+  }, []);
+*/
+  //// const counterState = selectCounter(store.getState(), counterId);
+  return (
+    <>
+     counter {counterState?.counter}
+        <button
+          onClick={() =>
+            // store.dispatch({
+            dispatch({
+              type: 'increment',
+              payload: {counterId}
+            } satisfies IncrementAction)
+          }
+        >
+          increment
+        </button>
+        <button onClick={() => store.dispatch({type: 'decrement', payload: {counterId}} satisfies DecrementAction)}>
+          decrement
+        </button>
+    </>
+  )
+}
 export default App
--- src/store.ts
+++ src/store.ts
@@ -1,37 +1,67 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { useSelector, useDispatch, useStore } from "react-redux";
+
+type CounterState = {
+  counter: number;
+};
+export type CounterId = string;
 
 // описываем структуру состояния
 type State = {
-  counter: number,
+  counters: Record<CounterId, CounterState | undefined>;
 } // store  в котором хранится state
 
 
 export type IncrementAction = {
-  type: 'increment'
+  type: 'increment';
+  payload: {
+    counterId: CounterId;
+  }
 }
 export type DecrementAction = {
-  type: 'decrement'
+  type: 'decrement';
+  payload: {
+    counterId: CounterId;
+  }
 }
 // типы action-ов
 type Action = IncrementAction | DecrementAction;
 
-
-const initialState = {
-  counter: 0,
+const initialCounterState: CounterState = {counter: 0};
+const initialState: State = {
+  counters: {},
 }
 // создание reducer
 const reducer = (state = initialState, action: Action) : State => { // в первый раз состояния не будет
   switch(action.type) {
-    case "increment":
+    case "increment": {
+      const {counterId} = action.payload;
+      const currentCounter = state.counters[counterId] ?? initialCounterState;
       return {
         ...state, // все остальные поля
-        counter: state.counter + 1,
+        counters: {
+          ...state.counters,
+          [counterId]: {
+            ...currentCounter,
+            counter: currentCounter.counter + 1
+          }
+        }
       };
-    case "decrement":
+    }
+    case "decrement": {
+      const {counterId} = action.payload;
+      const currentCounter = state.counters[counterId] ?? initialCounterState;
       return {
-        ...state,
-        counter: state.counter - 1,
+        ...state, // все остальные поля
+        counters: {
+          ...state.counters,
+          [counterId]: {
+            ...currentCounter,
+            counter: currentCounter.counter - 1
+          }
+        }
       };
+    }
     default:
       return state;
   }
@@ -42,7 +72,16 @@ export const store = configureStore({
   reducer: reducer,
 });
 
-store.dispatch;
-store.getState;
-store.subscribe;
+export const selectCounter = (state: AppState, counterId: CounterId) => state.counters[counterId];
 
+// экспорт type Store
+export type AppState = ReturnType<typeof store.getState>;
+// store.dispatch;
+// store.getState;
+// store.subscribe;
+
+export type AppDispatch = typeof store.dispatch;
+// возвращает версию useSelector уже типизированную AppStat-ом
+export const useAppSelector = useSelector.withTypes<AppState>();
+export const useAppDispatch = () => useDispatch.withTypes<AppDispatch>();
+export const useAppStore = useStore.withTypes<typeof store>();
